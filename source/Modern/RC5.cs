9f7c8a1ddc0724f8ac9dc96cef60a410f0935b5d
Zacatov Andrei <andrei.zacatov@isa.utm.md>
2022-11-03 01:52:51 +0200

    Scrap Blowfish;Start RC5

---
+++ source/Modern/RC5.cs
@@ -0,0 +1,17 @@
+using System.Text
+namespace Ciphers;
+class RC5 : Cipher
+{
+
+
+
+    public override string Encode(string plain)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string Decode(string cipher)
+    {
+        throw new NotImplementedException();
+    }
+}
