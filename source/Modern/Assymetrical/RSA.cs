8a733e53e8c24e9d41f0d85b42753c721689e9f4
Zacatov Andrei <andrei.zacatov@isa.utm.md>
2022-11-04 23:02:20 +0200

    Start work on Lab3

---
+++ source/Modern/Assymetrical/RSA.cs
@@ -0,0 +1,15 @@
+using System.Numerics;
+namespace Ciphers;
+
+// This is the sample implementation for the RSA cipher
+// it uses a pregenerated pair of 2 20-digit long primes 
+
+
+
+
+public class RSA
+{
+    //pregenerated primes from some website i found
+    private string Pstr = "174151584580014149441322636991";
+    private string Qstr = "861745732739716482352531472803";
+}
